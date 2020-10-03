136a573449508f021e278fbf5f901cbd83a3e6f0
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-10-03 08:42:35 +0300

    edit: Sorting added
    Signed-off-by: Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
    
    Took 8 minutes

--- src/main/java/jsonLoader/Main.java
+++ src/main/java/jsonLoader/Main.java
@@ -2,14 +2,21 @@ package jsonLoader;
 
 import MoviePortal.Personality;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Personality> personalities = JsonDealer.loadPersonality();
+        personalities.sort(new Comparator<Personality>() {
+            @Override
+            public int compare(Personality o1, Personality o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
         System.out.println(personalities);
-        Personality personality = JsonDealer.findPersonByName(personalities, "Джет Ли");
-        personality.setName("Акакий Акакиевич");
+//        Personality personality = JsonDealer.findPersonByName(personalities, "Джет Ли");
+//        personality.setName("Акакий Акакиевич");
         JsonDealer.saveAll(personalities);
     }
 }
