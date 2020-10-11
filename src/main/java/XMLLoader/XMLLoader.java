4b06e635a5e65c52ccbfc2b62391f97ad377ca2f
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-10-11 11:47:59 +0300

    edit: added sorting and searching
    Signed-off-by: Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
    
    Took 14 minutes

--- src/main/java/XMLLoader/XMLLoader.java
+++ src/main/java/XMLLoader/XMLLoader.java
@@ -11,26 +11,32 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class XMLLoader {
     public static void main(String[] args) {
         ArrayList<User> userSet = new ArrayList<User>();
         userSet.add(new User(
-                "Алекс",
-                new ArrayList<Rate>(),
-                "mail@example.com",
-                "asdad"));
-        userSet.add(new User(
                 "Маша",
                 new ArrayList<Rate>(),
                 "masha@example.com",
                 "klkwgualsbrg"));
+        userSet.add(new User(
+                "Алекс",
+                new ArrayList<Rate>(),
+                "mail@example.com",
+                "asdad"));
         ArrayList<Movie> movieSet = new ArrayList<Movie>();
         MoviePortal portal = new MoviePortal("КиноПоиск", userSet, movieSet);
         save(portal);
 
         MoviePortal unmarshalled = load();
+        ArrayList<User> sorted = (ArrayList<User>) unmarshalled.getUsers();
+        sorted.sort(Comparator.comparing(User::getName));
         System.out.println(unmarshalled);
+        System.out.println("\n\n" + sorted);
+
+        System.out.println("\n\n" +findByName(sorted, "Маша"));
     }
 
     public static void save(MoviePortal portal) {
@@ -53,6 +59,16 @@ public class XMLLoader {
             return (MoviePortal) unmarshaller.unmarshal(new File("portal.xml"));
         } catch (JAXBException e) {
             e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static User findByName(ArrayList<User> users, String name) {
+        for (User user :
+                users) {
+            if (user.getName().equals(name)) {
+                return user;
+            }
         }
         return null;
     }
