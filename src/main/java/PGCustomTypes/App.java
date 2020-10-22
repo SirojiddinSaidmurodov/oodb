2ffa802466188c58d5f3c496e32890f4e73429f1
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-10-22 09:36:02 +0300

    add: update execurion
    Signed-off-by: SirojiddinSaidmurodov <siroj.serj15@outlook.com>
    
    Took 18 minutes

--- src/main/java/PGCustomTypes/App.java
+++ src/main/java/PGCustomTypes/App.java
@@ -17,19 +17,30 @@ public class App {
         assert connection != null;
         addData(connection);
         showData(connection);
+        editData(connection);
+        System.out.println(
+                "\n                 Updated the DB\n======================================================================\n");
+        showData(connection);
     }
 
     public static void addData(Connection connection) {
         try {
             PreparedStatement statement = connection.prepareStatement(
                     "INSERT INTO actors (person, role) VALUES (row (?,?,?),?)");
-            statement.setString(1, "Иван");
+            statement.setString(1, "Иван Ургант");
             GregorianCalendar calendar = new GregorianCalendar();
             calendar.set(1998, Calendar.DECEMBER, 25);
             statement.setDate(2, new Date(calendar.getTimeInMillis()));
-            statement.setString(3, "Популярный актер");
-            statement.setString(4, "Палка");
+            statement.setString(3, "российский актёр, шоумен, теле- и радиоведущий, певец, музыкант, композитор, продюсер и сценарист. Ведущий программы «Вечерний Ургант» на «Первом канале»");
+            statement.setString(4, "Самого себя");
             statement.executeUpdate();
+            calendar = new GregorianCalendar();
+            calendar.set(1986, Calendar.MAY, 13);
+            statement.setDate(2, new Date(calendar.getTimeInMillis()));
+            statement.setString(3, "британский актёр, фотомодель и музыкант. Мировую известность ему принесли роли Седрика Диггори в фильме «Гарри Поттер и Кубок огня» и Эдварда Каллена в серии фильмов «Сумерки»");
+            statement.setString(4, "Вампира из будущего");
+            statement.executeUpdate();
+
         } catch (SQLException throwable) {
             throwable.printStackTrace();
         }
@@ -45,9 +56,23 @@ public class App {
                 System.out.println(resultSet.getString(1) +
                         ",\n    Дата рождения: " + resultSet.getDate(2) +
                         ",\n    Биография: " + resultSet.getString(3) +
-                        ",\n    Роль в фильме: " + resultSet.getString(4) + "\n"
+                        ",\n    В роли: " + resultSet.getString(4) + "\n"
                 );
             }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void editData(Connection connection) {
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(
+                    "UPDATE actors set person.dateOfBirth = ? where (person).name LIKE '%ван%'");
+            GregorianCalendar calendar = new GregorianCalendar();
+            calendar.set(1976, Calendar.JANUARY, 30);
+            statement.setDate(1, new Date(calendar.getTimeInMillis()));
+            statement.execute();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
