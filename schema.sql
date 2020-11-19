eba4c5acecbf9966f7f8a50fc7d0684ff3215b8f
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-11-19 09:35:25 +0300

    edit: added foreign keys and some new entities
    Signed-off-by: SirojiddinSaidmurodov <siroj.serj15@outlook.com>
    
    Took 30 minutes

--- schema.sql
+++ schema.sql
@@ -20,16 +20,32 @@ DROP TABLE IF EXISTS actor;
 CREATE TABLE actor
 (
     id        SERIAL,
+    role      char(255),
     person_id BIGINT,
-    PRIMARY KEY (id)
+    movie_id  BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (person_id) REFERENCES personality (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (movie_id) REFERENCES movie (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS artist;
 CREATE TABLE artist
 (
-    id        SERIAL,
-    person_id BIGINT,
-    PRIMARY KEY (id)
+    id         SERIAL,
+    occupation char(255),
+    person_id  BIGINT,
+    movie_id   BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (person_id) REFERENCES personality (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (movie_id) REFERENCES movie (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS movie;
@@ -44,6 +60,25 @@ CREATE TABLE movie
 DROP TABLE IF EXISTS usr;
 CREATE TABLE usr
 (
+    id    SERIAL,
     name  char(255),
-    eMail char(25)
+    eMail char(25),
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS rate;
+CREATE TABLE rate
+(
+    id           SERIAL,
+    value        integer,
+    movie_id     bigint,
+    dateOfChange date,
+    usr_id       bigint,
+    PRIMARY KEY (id),
+    FOREIGN KEY (movie_id) REFERENCES movie (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (usr_id) REFERENCES usr (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
