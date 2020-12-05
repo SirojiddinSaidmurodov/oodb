3a055417e658e4d663d10ffde53dbb16cb8cade5
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-12-05 03:47:57 +0300

    edit: implemented findAll method
    Signed-off-by: SirojiddinSaidmurodov <siroj.serj15@outlook.com>
    
    Took 3 hours 23 minutes

--- src/main/java/ORMManagement/EntityManager.java
+++ src/main/java/ORMManagement/EntityManager.java
@@ -6,16 +6,32 @@
 
 package ORMManagement;
 
+import ObjModelAnalysis.annotations.ManyToOne;
+import ObjModelAnalysis.annotations.OneToMany;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class EntityManager implements IEntityManager<Long> {
     private Connection connection;
+    private HashMap<String, HashSet<String>> tables;
 
     public EntityManager(Connection connection) {
         this.connection = connection;
+    }
+
+    public void setTables(HashMap<String, HashSet<String>> tables) {
+        this.tables = tables;
     }
 
     @Override
@@ -42,9 +58,56 @@ public class EntityManager implements IEntityManager<Long> {
     }
 
     @Override
-    public List<Entity<Long>> findAll() {
+    public List<Entity<Long>> findAll(Class<?> entityClass) {
+        ArrayList<Entity<Long>> result = new ArrayList<>();
+        String tableName = entityClass.getSimpleName().toLowerCase();
 
-        return null;
+        StringBuilder builder = new StringBuilder();
+        ArrayList<Field> fields = new ArrayList<>();
+        for (Field declaredField : entityClass.getDeclaredFields()) {
+            if (declaredField.isAnnotationPresent(OneToMany.class)) {
+
+            } else if (declaredField.isAnnotationPresent(ManyToOne.class)) {
+
+            } else {
+                String s = declaredField.getName().toLowerCase();
+                builder.append(s);
+                fields.add(declaredField);
+            }
+            builder.append(",");
+        }
+        builder.deleteCharAt(builder.lastIndexOf(","));
+        String columns = builder.toString();
+        PreparedStatement selectStatement = null;
+        try {
+            selectStatement = connection.prepareStatement("SELECT " + columns + " FROM " + tableName);
+            ResultSet resultSet = selectStatement.executeQuery();
+
+            Constructor<?> constructor = entityClass.getDeclaredConstructor(null);
+            while (resultSet.next()) {
+                Object entity = constructor.newInstance(null);
+                for (int i = 0; i < fields.size(); i++) {
+                    String name = fields.get(i).getName();
+                    name = "set" + name.substring(0, 1).toUpperCase() + name.substring(1);
+                    Class<?> type = fields.get(i).getType();
+                    Method setter = entityClass.getMethod(
+                            name,
+                            type);
+                    setter.invoke(entity, resultSet.getObject(i+1, type));
+                }
+                result.add((Entity<Long>) entity);
+            }
+        } catch (SQLException | NoSuchMethodException throwables) {
+            throwables.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
