58b75ce9df2c6eb75d46d27f09906f2d85132f18
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2020-12-05 03:52:07 +0300

    edit: removed unnecessary test
    Signed-off-by: SirojiddinSaidmurodov <siroj.serj15@outlook.com>
    
    Took 47 seconds

--- src/test/java/ORMManagement/EntityManagerFactoryTest.java
+++ src/test/java/ORMManagement/EntityManagerFactoryTest.java
@@ -6,24 +6,6 @@
 
 package ORMManagement;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
 class EntityManagerFactoryTest {
 
-    @Test
-    void is() {
-        Properties properties = new Properties();
-        try {
-            properties.load(new FileReader("src/main/resources/connection.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        EntityManagerFactory factory = new EntityManagerFactory(properties);
-        Assertions.assertTrue(factory.isDbValid());
-    }
 }
